d1e91383c7ac3e3fa46bf8b70b5bcef7f37c26b3
kostopez <stavroskostopoulos@outlook.com>
2024-08-19 17:59:34 +0300

    Homepage banner start

--- betsson-clone/src/App.js
+++ betsson-clone/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 //components
 import Appbar from './Components/Layout/Appbar/Appbar';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Appbar />
+      <Home />
     </div>
   );
 }
